dab1bc9f726284e448595eb96594d715331abf79
LetMeCall Corp <licensing@letmecall.com>
2019-01-10 14:02:21 +0530

    Initial Commit

---
+++ SelfSizingFoLabel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfSizingFoLabel
+//
+//  Created by LetMeCall Corp on 10/01/19.
+//  Copyright © 2019 LetMeCall Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
